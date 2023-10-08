3cdf7cc78e97fe6dc703868c6624de1abe4c231f
Kyllian Mobley <kyllian@users.noreply.github.com>
2023-10-08 20:33:23 +0000

    Add todo comment to pre-install.sh

--- eos/pre-install/pre-install.sh
+++ eos/pre-install/pre-install.sh
@@ -1,2 +1,4 @@
 # When the live environment starts, we need some of the tools in the repo.
 # git clone https://github.com/kyllian/sud-linux.git ~/sud-linux/
+
+# TODO: Replace the content in default user_pkglist.txt and user_commands.bash with our own.
