e40021525ec04f62c387fb7bf29e887fac740aed
kyllian <5831233+kyllian@users.noreply.github.com>
2023-10-17 17:22:13 -0500

    Remove protonvpn-cli. It was deprecated.

--- eos/post-install/aur-programs/configure.sh
+++ eos/post-install/aur-programs/configure.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
 DIR=~/Source/Repos/sud-linux/eos/post-install/aur-programs
-
-# Install protonvpn-cli
-bash $DIR/protonvpn-cli/configure.sh
--- eos/post-install/aur-programs/protonvpn-cli/configure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR=~/Source/Repos/sud-linux/eos/post-install/aur-programs/protonvpn-cli
-
-BRC=$DIR/.bashrc
-cat "$BRC" >> ~/.bashrc
-
-# echo "Logging in to ProtonVPN is necessary before you can connect to the internet."
